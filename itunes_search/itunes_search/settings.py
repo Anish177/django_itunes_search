82e6846f43512c85e0a1a24e113e1f065e792864
Anish177 <30626499+Anish177@users.noreply.github.com>
2024-08-28 19:39:10 +0530

    Update settings.py

--- itunes_search/itunes_search/settings.py
+++ itunes_search/itunes_search/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q(f%3qt=-ltit_+zsx7h#e0@hn*v6#-2_81rr#-1q@48+6x%&p'
+SECRET_KEY = hiddem
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
